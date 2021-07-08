8494cf96e08ce16621a8dc935844c22a1a06440f
datafluisteraar <steven.gort@ictu.nl>
2021-07-08 15:12:15 +0200

    added morgan

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const caseRoutes = require('./api/routes/cases');
 
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/cases', caseRoutes);
 
